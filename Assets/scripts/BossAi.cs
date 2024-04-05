2f606a1fe87cc5fc1b5e45c20bf92b46fd513802
flopmann <flop5899@gmail.com>
2024-04-05 15:05:55 +0200

    lavede ranged attack

--- Assets/scripts/BossAi.cs
+++ Assets/scripts/BossAi.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+
 
 public class BossAi : MonoBehaviour
 {
@@ -12,7 +14,18 @@ public class BossAi : MonoBehaviour
     public float timeTillStart = 10f;
     public float timeTillChase = 3f;
     public Animator animator;
-    public float timeTillAttack = 2f;
+    public float timeTillAttack = 1.5f;
+    public float rangedRange = 20f;
+
+    public bool stage1;
+    bool rangedTargetSpawned;
+    bool meleeTargetSpawned;
+
+    public float puzzleHealth = 100f;
+
+    bool hasPicked;
+
+    private int meleeOrRanged;
 
     public enum BossState
     {
@@ -20,18 +33,21 @@ public class BossAi : MonoBehaviour
         Idle, 
         Meleeattacking, 
         Rangedattacking,
+        
     }
     void Start()
     {
         Currentstate = BossState.Idle;
         animator = GetComponent<Animator>();
-        transform.forward = Target.position;
+        //stage1 = true;
+        rangedTargetSpawned = false;
+        hasPicked = false;
+        meleeTargetSpawned = false;
     }
 
    
     void Update()
     {
-        
         switch (Currentstate)
         {
             case BossState.Idle:
@@ -47,6 +63,11 @@ public class BossAi : MonoBehaviour
                  rangedAttacking();
                  break;
         }
+        if (puzzleHealth <= 0)
+        {
+            stage1 = false;
+          
+        }
     }
 
     void idleState()
@@ -60,14 +81,42 @@ public class BossAi : MonoBehaviour
     
     void moving()
     {
+       
+        
         Debug.Log(Currentstate);
         GetComponent<AIMove>().moveAi();
-        timeTillChase = 3f;
+        timeTillChase = 10f;
         timeTillAttack = 3f;
+
+         
+
+        if (stage1 == false) 
+        {
+            if (hasPicked == false)
+            {
+                meleeOrRanged = UnityEngine.Random.Range(1, 3);
+                Debug.Log(meleeOrRanged);
+                hasPicked = true;
+            }
+
+            if ((Target.transform.position - transform.position).magnitude <= rangedRange && meleeOrRanged == 1)
+            {
+                Currentstate = BossState.Rangedattacking;
+            }
+            
+           if ((Target.transform.position - transform.position).magnitude <= meleeRange && meleeOrRanged == 2)
+            {
+                Currentstate = BossState.Meleeattacking;
+            }
+
+        }
+
         if ((Target.transform.position - transform.position).magnitude <= meleeRange)
         {
             Currentstate = BossState.Meleeattacking;
         }
+        
+
     }
     void meleeAttacking()
     {
@@ -77,21 +126,56 @@ public class BossAi : MonoBehaviour
             timeTillChase -= Time.deltaTime;
             if(timeTillChase <= 0f)
             {
+                if(stage1 == false)
+                {
+                    hasPicked = false;
+                    Currentstate = BossState.Moving;
+                }
                 Currentstate = BossState.Moving;
+                
             }
         }
+        
         timeTillAttack -= Time.deltaTime;
-
-        if (timeTillAttack <= 0f)
+        if (timeTillAttack <= 0)
         {
+            GetComponent<MeleeAttack>().spawnTarget();
+            meleeTargetSpawned = true;
+
+        }
+        if (meleeTargetSpawned == true)
+        {
+            meleeTargetSpawned = false;
             GetComponent<MeleeAttack>().meleeAttack();
-            timeTillAttack = 3f;
-            
+            timeTillAttack = 5f;
+            GetComponent<AIMove>().currentSpeed = 0f;
         }
        
     }
     void rangedAttacking()
     {
+        Debug.Log("Attacking");
+        timeTillAttack -= Time.deltaTime;
 
+        if ((Target.transform.position - transform.position).magnitude >= rangedRange)
+        {
+            hasPicked = false;
+            Currentstate = BossState.Moving;
+        }
+
+        if(timeTillAttack <= 0)
+        {
+            GetComponent<rangedAttack>().spawnTarget();
+            rangedTargetSpawned = true;
+        }
+        if (rangedTargetSpawned == true)
+        {
+
+            timeTillAttack = 3;
+            rangedTargetSpawned = false;
+            GetComponent<rangedAttack>().spawnHitBox();
+
+
+        }
     }
 }
--- Assets/scripts/attackMovement.cs
+++ Assets/scripts/attackMovement.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class attackMovement : MonoBehaviour
 {
     private Vector3 Target;
     public Transform attackTarget;
-    public float attackSpeed = 5f;
+    public float attackSpeed = 80f;
+    public int attackDamage = 25;
 
-
+    public static int Health = 200;
     void Update()
     {
         attackMove();
+        
     }
     public void attackMove()
     {
@@ -20,10 +23,21 @@ public class attackMovement : MonoBehaviour
         Vector3 newPosition = Vector3.MoveTowards(transform.position, Target, attackSpeed*Time.deltaTime);
         transform.position = newPosition;
 
-        if (transform.position == attackTarget.position)
-        {
-            Destroy(gameObject);
-        }
+        //if (transform.position == attackTarget.position)
+        //{
+          //  Destroy(gameObject);
+        //}
 
     }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "Player")
+        {
+            Health = Health - attackDamage;
+            Destroy(gameObject);
+            Debug.Log(Health);
+        }
+    }
+
 }
