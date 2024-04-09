d30d016e556c1e03783bbf7be98e0b91804a823f
flopmann <flop5899@gmail.com>
2024-04-09 14:00:19 +0200

    lavede puzzel

--- Assets/scripts/Puzzle.cs
+++ Assets/scripts/Puzzle.cs
@@ -19,16 +19,21 @@ public class Puzzle : MonoBehaviour
     public GameObject Symbol2;
     public GameObject Symbol3;
     public GameObject Symbol4;
+    [Header("Symbol Health")]
+    public static int Symbol1Health = 400;
+    public static int Symbol2Health = 400;
+    public static int Symbol3Health = 400;
+    public static int Symbol4Health = 400;
 
 
     private GameObject currentSymbolBoss;  
     private GameObject currentSymbolStage;
 
-    private int currentSymbol;
+    public static int currentSymbol;
 
     bool activeSymbol;
 
-    private float timeTillNewSymbol = 5;
+    private float timeTillNewSymbol = 10f;
 
 
 
@@ -40,21 +45,25 @@ public class Puzzle : MonoBehaviour
     
     void Update()
     {
-        if (activeSymbol == false)
+        if (timeTillNewSymbol <= 0f)
         {
             chooseSymbol();
             activeSymbol = true;
-            timeTillNewSymbol = 5;  
+            timeTillNewSymbol = 10f;  
         }
         if (activeSymbol == true)
         {
-            timeTillNewSymbol -= Time.deltaTime;
-        }
-        if (timeTillNewSymbol <= 0)
-        {
+            activateSymbol();
             activeSymbol = false;
         }
-
+        if (activeSymbol == false)
+        {
+            timeTillNewSymbol -= Time.deltaTime;
+        }
+        if (timeTillNewSymbol <= 1f)
+        {
+            deActivateSymbol();
+        }
     }
     void chooseSymbol()
     {
@@ -66,25 +75,65 @@ public class Puzzle : MonoBehaviour
     {
         if (currentSymbol == 1)
         {
-            currentSymbolBoss = Instantiate(Symbol1, symbol1Boss.position, Quaternion.identity);
-            currentSymbolStage = Instantiate(Symbol1, symbol1Stage.position, Quaternion.identity);
+            if (Symbol1Health <= 400)
+            {
+                currentSymbolBoss = Instantiate(Symbol1, symbol1Boss.position, Quaternion.identity);
+                currentSymbolStage = Instantiate(Symbol1, symbol1Stage.position, Quaternion.identity);
+            }
+            if (Symbol1Health <= 0)
+            {
+                activeSymbol = false;
+            }
         }
         if (currentSymbol == 2)
         {
-            currentSymbolBoss = Instantiate(Symbol2, symbol1Boss.position, Quaternion.identity);
-            currentSymbolStage = Instantiate(Symbol2, symbol1Stage.position, Quaternion.identity);
+            if (Symbol2Health <= 400)
+            {
+                currentSymbolBoss = Instantiate(Symbol2, symbol2Boss.position, Quaternion.identity);
+                currentSymbolStage = Instantiate(Symbol2, symbol2Stage.position, Quaternion.identity);
+            }
+            if (Symbol2Health <= 0)
+            {
+                activeSymbol = false;
+            }
         }
         if (currentSymbol == 3)
         {
-            currentSymbolBoss = Instantiate(Symbol3, symbol1Boss.position, Quaternion.identity);
-            currentSymbolStage = Instantiate(Symbol3, symbol1Stage.position, Quaternion.identity);
+            if (Symbol3Health <= 400)
+            {
+                currentSymbolBoss = Instantiate(Symbol3, symbol3Boss.position, Quaternion.identity);
+                currentSymbolStage = Instantiate(Symbol3, symbol3Stage.position, Quaternion.identity);
+            }
+            if (Symbol3Health <= 0)
+            {
+                activeSymbol = false;
+            }
         }
         if (currentSymbol == 4)
         {
-            currentSymbolBoss = Instantiate(Symbol4, symbol1Boss.position, Quaternion.identity);
-            currentSymbolStage = Instantiate(Symbol4, symbol1Stage.position, Quaternion.identity);
+            if (Symbol4Health <= 400)
+            {
+                currentSymbolBoss = Instantiate(Symbol4, symbol4Boss.position, Quaternion.identity);
+                currentSymbolStage = Instantiate(Symbol4, symbol4Stage.position, Quaternion.identity);
+            }
+            if (Symbol4Health <= 0)
+            {
+                activeSymbol = false;
+            }
         }
 
     }
+    void deActivateSymbol()
+    {
+        //Destroy(Symbol1);
+        //Destroy(Symbol2);
+        //Destroy(Symbol3);
+        //Destroy(Symbol4);
+        Destroy(currentSymbolBoss);
+        Destroy(currentSymbolStage);
+
+    }
+
+    
 
 }
