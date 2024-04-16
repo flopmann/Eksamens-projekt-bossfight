bcdd7b7dfa4101b63e39b1bf0f4c4eb0ca61a14b
flopmann <flop5899@gmail.com>
2024-04-16 14:06:32 +0200

    win Scene

--- Assets/scripts/BossHealth.cs
+++ Assets/scripts/BossHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossHealth : MonoBehaviour
 {
@@ -33,6 +34,7 @@ public class BossHealth : MonoBehaviour
         if (deathTimer <= 0f)
         {
             Destroy(gameObject);
+            SceneManager.LoadScene("WinScene");
         }
     }
 
