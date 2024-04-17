22b529c3c796a6316ed8c90bb38cd64b57945638
uwuJaver <ieva.stonciute@gmail.com>
2024-04-17 15:37:31 +0200

    platform 0

---
+++ Assets/scripts/PlatformMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BossHealth.health <= 0)
+        {
+            movePlatform();
+        }
+
+    }
+
+    void movePlatform()
+    {
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        transform.position = newPosition;
+    }
+}
