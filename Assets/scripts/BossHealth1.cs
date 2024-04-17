6a11d090d079147a9554156a1433a600f5611665
flopmann <flop5899@gmail.com>
2024-04-17 11:04:12 +0200

    HUD

---
+++ Assets/scripts/BossHealth1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class BossHealthDisplay : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI BossHealthText;
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        BossHealthText.text ="Boss health" + BossHealth.health.ToString();
+    }
+}
