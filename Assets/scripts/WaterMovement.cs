4d68331f31e87ef0ab88f25c2a37b555f665dfcc
flopmann <flop5899@gmail.com>
2024-04-17 09:21:56 +0200

    Phase 3 done

--- Assets/scripts/WaterMovement.cs
+++ Assets/scripts/WaterMovement.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WaterMovement : MonoBehaviour
 {
@@ -11,28 +10,59 @@ public class WaterMovement : MonoBehaviour
     public Transform Target;
 
     public float air = 100;
+
+    private float timeTillDrown = 1;
+    private float timeTillLoseAir = 1;
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
         moveWater();
+        Drowning();
+        if (transform.position.y < PlayerHead.position.y)
+        {
+            air = 100;
+        }
+        if (MeleeAttack.Health <= 0)
+        {
+            SceneManager.LoadScene("loseScene");
+        }
+        
     }
 
     void moveWater()
     {
-        Vector3 newPosition = Vector3.MoveTowards(transform.position, Target.position, speed*Time.deltaTime);
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, Target.position, speed * Time.deltaTime);
 
         transform.position = newPosition;
     }
     void Drowning()
     {
-        if (transform.position.y < PlayerHead.position.y)
+        if (transform.position.y > PlayerHead.position.y)
         {
-            air -= Time.deltaTime;
+            timeTillLoseAir -= Time.deltaTime;
+            if (timeTillLoseAir <= 0)
+            {
+                air = air - 20;
+                timeTillLoseAir = 1;
+                Debug.Log(air);
+            }
+
+            if (air <= 0)
+            {
+                timeTillDrown -= Time.deltaTime;
+                if (timeTillDrown <= 0)
+                {
+                    MeleeAttack.Health = MeleeAttack.Health - 10;
+                    timeTillDrown = 1;
+                    Debug.Log(MeleeAttack.Health);
+                }
+
+            }
         }
     }
 }
